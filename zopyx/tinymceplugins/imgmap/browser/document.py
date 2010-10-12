3f42bfa1618087db955e128b1d6add324f76d38f
ajung <ajung@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-10-12 10:55:48 +0000

    fixes

--- zopyx/tinymceplugins/imgmap/browser/document.py
+++ zopyx/tinymceplugins/imgmap/browser/document.py
@@ -1,4 +1,5 @@
 
+import demjson
 import BeautifulSoup
 from Products.Five.browser import BrowserView
 
@@ -8,17 +9,18 @@ class AnchorView(BrowserView):
 
 
     def __call__(self):
+        """ return all anchors """
 
         html = self.context.getText()
         result = list()
         soup = BeautifulSoup.BeautifulSoup(html)
-        for anchor in soup.find('a'):
+        for anchor in soup.findAll('a'):
             try:
                 name = anchor['name']
             except KeyError:
                 name = None
 
             if name:
-                result.append(dict(name=name, text=text))
-        return 
+                result.append(dict(name=name, text=name))
+        return demjson.encode(result) 
                                 
